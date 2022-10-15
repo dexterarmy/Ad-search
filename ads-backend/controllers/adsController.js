80da7891f924cee64d0f36b45cc39fc72c0581a1
dexterarmy <immohitsharma8@gmail.com>
2022-10-15 22:01:56 +0530

    made changes in aggregation pipeline

--- ads-backend/controllers/adsController.js
+++ ads-backend/controllers/adsController.js
@@ -23,55 +23,56 @@ exports.getAd = async (req, res, next) => {
   //   },
   // ]);
 
-  let companies;
+  // let companies;
 
-  if (req.params.keywords) {
-    companies = await Company.aggregate([
-      {
-        $match: { name: { $regex: req.params.keywords, $options: "i" } },
-      },
-    ]);
-  } else {
-    return res.status(400).json({ message: "please provide the input" });
-  }
+  // if (req.params.keywords) {
+  //   companies = await Company.aggregate([
+  //     {
+  //       $match: { $text: { $search: req.params.keywords } },
+  //     },
+  //   ]);
+  // } else {
+  //   return res.status(400).json({ message: "please provide the input" });
+  // }
 
-  if (companies.length !== 0) {
-    const companyData = await Ad.find({ companyId: companies[0]?._id });
-    return res.status(200).json({
-      status: "success",
-      body: companyData,
-    });
-  }
+  // if (companies.length !== 0) {
+  //   const companyData = await Ad.find({ companyId: companies[0]?._id });
+  //   return res.status(200).json({
+  //     status: "success",
+  //     body: companyData,
+  //   });
+  // }
 
+  // ad-search based on keywords typed
   const ads = await Ad.aggregate([
+    {
+      $lookup: {
+        from: "companies",
+        localField: "companyId",
+        foreignField: "_id",
+        as: "companyId",
+      },
+    },
+    {
+      $unwind: "$companyId",
+    },
     {
       $match: {
         $or: [
           { primaryText: { $regex: req.params.keywords, $options: "i" } },
           { headline: { $regex: req.params.keywords, $options: "i" } },
           { description: { $regex: req.params.keywords, $options: "i" } },
+          { "companyId.name": { $regex: req.params.keywords, $options: "i" } },
         ],
       },
     },
-
-    // {
-    //   $lookup: {
-    //     from: "companies",
-    //     localField: "companyId",
-    //     foreignField: "_id",
-    //     as: "companyId",
-    //   },
-    // },
-    // {
-    //   $unwind: "$companyId",
-    // },
   ]);
 
-  const arr = ads.map((ad) => ad._id);
-  const allAds = await Ad.find({ _id: { $in: arr } });
+  // const arr = ads.map((ad) => ad._id);
+  // const allAds = await Ad.find({ _id: { $in: arr } });
 
   res.status(200).json({
     status: "success",
-    body: allAds,
+    body: ads,
   });
 };
